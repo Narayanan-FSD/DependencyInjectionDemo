ac693b4a173559a0a2424fc3d9fcf47d20f3d3f0
Narayanan <narayanan_fullstackdeveloper@proton.me>
2025-11-23 21:33:02 +0530

    Configuration

--- DependencyInjectionDemo/DataAccessLayer/DataAccessService.cs
+++ DependencyInjectionDemo/DataAccessLayer/DataAccessService.cs
@@ -2,8 +2,15 @@
 {
     public class DataAccessService : IDataAccessService
     {
+        private readonly IConfiguration _configuration;
+        public DataAccessService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void DoDataBaseWork()
         {
+            var connString = _configuration.GetConnectionString("DefaultConnection");
+            Console.WriteLine(connString);
             Console.WriteLine("Database Operation");
         }
     }
